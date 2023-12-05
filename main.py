18a30c64292f4046aee5c78098487dad6c2fed8c
ADeVera412 <mrsalmon2008@gmail.com>
2023-12-05 10:52:40 -0800

    Push w/ README

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 #Sources: https://platform.openai.com/docs/quickstart?context=python
 #https://pypi.org/project/llm
 #https://www.youtube.com/watch?v=q5HiD5PNuck
-
+#https://github.com/openai/openai-python/discussions/742
 
 
 
@@ -12,56 +12,20 @@
 Using the OpenAI API, attempt to train the model on data from wookiepedia
 '''
 
-import openai
 
-openai.api_key = "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"
+from openai import OpenAI
+import os
 
-messages = [ {"role":"system", "role":"You are a helpful assistant"} ]
-
-while True:
-  message = input("User: ")
-  if message:
-    messages.append(
-      {"role": "user", "content": "message"}
-    )
-    chat = openai.Chatcompletion.create(
-      model = "gpt-3.5-turbo", messages=messages, temperature=0.5
-    )
-
-  reply = chat.choices[0].message.content
-  print(f"Chatgpt: {reply}")
-  messages.append({"role": "assistant", "content": reply})
+client = OpenAI
+completion = client.completions.create(model='curie')
+print(completion.choices[0].text)
+print(dict(completion).get('usage'))
+print(completion.model_dump_json(indent=2))
+  #os.environ['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'],  # this is also the default, it can be omitted
 
 
 
 
-
-
-
-
-
-
-
-'''
-def AI_bot(prompt):
-    response = openai.Chatcompletion.create(
-        model = "gpt-3.5-turbo",
-        messages = [{"role": "user", "content": prompt}]
-    )
-
-    return response.choices[0].message.content.strip()
-
-
-if __name__ == "__main__":
-    while True:
-      user_input = input("You: ")
-      if user_input.lower() in ["quit", "exit", "bye"]:
-        break
-
-
-response = AI_bot(user_input)
-print("Chatbot: ", response)
-'''
 
 
 
